3fdbc997ba30a2b551e1196a266f19d87ef3d53d
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2023-11-20 12:02:28 +0300

    Add files via upload

---
+++ arama örneği-826/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace arama_örneği_826
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAra = new System.Windows.Forms.Button();
+            this.txtAranan = new System.Windows.Forms.TextBox();
+            this.lbAdlar = new System.Windows.Forms.ListBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(143, 42);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 24);
+            this.btnAra.TabIndex = 0;
+            this.btnAra.Text = "ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // txtAranan
+            // 
+            this.txtAranan.Location = new System.Drawing.Point(1, 46);
+            this.txtAranan.Name = "txtAranan";
+            this.txtAranan.Size = new System.Drawing.Size(100, 20);
+            this.txtAranan.TabIndex = 1;
+            // 
+            // lbAdlar
+            // 
+            this.lbAdlar.FormattingEnabled = true;
+            this.lbAdlar.Items.AddRange(new object[] {
+            "hayrunnisa",
+            "sukeyna",
+            "eymen",
+            "kübra",
+            "efe",
+            "hayrunnisa",
+            "arda",
+            "rabia",
+            "sukeyna ",
+            "deniz",
+            "hayrunnisa",
+            "ömer"});
+            this.lbAdlar.Location = new System.Drawing.Point(48, 100);
+            this.lbAdlar.Name = "lbAdlar";
+            this.lbAdlar.Size = new System.Drawing.Size(120, 199);
+            this.lbAdlar.TabIndex = 2;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(45, 341);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(42, 13);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "...kişiler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 450);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.lbAdlar);
+            this.Controls.Add(this.txtAranan);
+            this.Controls.Add(this.btnAra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox txtAranan;
+        private System.Windows.Forms.ListBox lbAdlar;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
